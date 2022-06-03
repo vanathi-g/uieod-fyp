63c65d5ef6ab43a8a685b57dcc3d07291d5fcabf
Sruthi Bijoy <78639205+sruthibijoyr@users.noreply.github.com>
2022-06-03 19:29:23 +0530

    Add files via upload

---
+++ YOLOv3/delete_extra_test.py
@@ -0,0 +1,24 @@
+import json
+import os
+
+file = open("instances_test.json")
+instances = json.load(file)
+
+images = {}
+
+for i in instances['annotations']:
+	if(i['image_id'] not in images):
+		images[i['image_id']] = {i['category_id'] : [i['bbox']]}
+	else:
+		if i['category_id'] not in images[i['image_id']]:
+			images[i['image_id']][i['category_id']] = [i['bbox']]
+		else:
+			images[i['image_id']][i['category_id']].append(i['bbox'])
+
+temp = [i for i in range(1,1112)]
+for id in images:
+	if id in temp:
+		temp.remove(id)
+print(len(temp))
+for i in temp:
+	os.remove(str(i)+".jpg")
